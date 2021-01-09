49abb9a16d1661ef9f76f860c0fe8181ced15e7d
Gautam2000 <63543004+Gautam-MV@users.noreply.github.com>
2021-01-10 00:45:59 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,31 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
 class Preprocessor:
-    """
-        This class shall  be used to clean and transform the data before training.
-
-        Written By: iNeuron Intelligence
-        Version: 1.0
-        Revisions: None
-
-        """
+  
 
     def __init__(self, file_object, logger_object):
         self.file_object = file_object
         self.logger_object = logger_object
 
     def remove_columns(self,data,columns):
-        """
-                Method Name: remove_columns
-                Description: This method removes the given columns from a pandas dataframe.
-                Output: A pandas DataFrame after removing the specified columns.
-                On Failure: Raise Exception
-
-                Written By: iNeuron Intelligence
-                Version: 1.0
-                Revisions: None
-
-        """
+     
         self.logger_object.log(self.file_object, 'Entered the remove_columns method of the Preprocessor class')
         self.data=data
         self.columns=columns
@@ -42,17 +25,7 @@ class Preprocessor:
             raise Exception()
 
     def separate_label_feature(self, data, label_column_name):
-        """
-                        Method Name: separate_label_feature
-                        Description: This method separates the features and a Label Coulmns.
-                        Output: Returns two separate Dataframes, one containing features and the other containing Labels .
-                        On Failure: Raise Exception
-
-                        Written By: iNeuron Intelligence
-                        Version: 1.0
-                        Revisions: None
-
-                """
+     
         self.logger_object.log(self.file_object, 'Entered the separate_label_feature method of the Preprocessor class')
         try:
             self.X=data.drop(labels=label_column_name,axis=1) # drop the columns specified and separate the feature columns
@@ -66,17 +39,7 @@ class Preprocessor:
             raise Exception()
 
     def is_null_present(self,data):
-        """
-                                Method Name: is_null_present
-                                Description: This method checks whether there are null values present in the pandas Dataframe or not.
-                                Output: Returns a Boolean Value. True if null values are present in the DataFrame, False if they are not present.
-                                On Failure: Raise Exception
-
-                                Written By: iNeuron Intelligence
-                                Version: 1.0
-                                Revisions: None
-
-                        """
+  
         self.logger_object.log(self.file_object, 'Entered the is_null_present method of the Preprocessor class')
         self.null_present = False
         try:
@@ -98,16 +61,7 @@ class Preprocessor:
             raise Exception()
 
     def impute_missing_values(self, data):
-        """
-                                        Method Name: impute_missing_values
-                                        Description: This method replaces all the missing values in the Dataframe using KNN Imputer.
-                                        Output: A Dataframe which has all the missing values imputed.
-                                        On Failure: Raise Exception
-
-                                        Written By: iNeuron Intelligence
-                                        Version: 1.0
-                                        Revisions: None
-                     """
+  
         self.logger_object.log(self.file_object, 'Entered the impute_missing_values method of the Preprocessor class')
         self.data= data
         try:
@@ -123,16 +77,7 @@ class Preprocessor:
             raise Exception()
 
     def get_columns_with_zero_std_deviation(self,data):
-        """
-                                                Method Name: get_columns_with_zero_std_deviation
-                                                Description: This method finds out the columns which have a standard deviation of zero.
-                                                Output: List of the columns with standard deviation of zero
-                                                On Failure: Raise Exception
-
-                                                Written By: iNeuron Intelligence
-                                                Version: 1.0
-                                                Revisions: None
-                             """
+    
         self.logger_object.log(self.file_object, 'Entered the get_columns_with_zero_std_deviation method of the Preprocessor class')
         self.columns=data.columns
         self.data_n = data.describe()
